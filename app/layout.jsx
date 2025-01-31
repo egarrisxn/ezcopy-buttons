5d9636fb179e96d035d8db9da7eb82f52df18d6d
egarrisxn <egarrisxn@gmail.com>
2025-01-31 18:35:25 -0500

    og stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,8 +13,65 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: 'Ez Cpy Btns',
-  description: 'Copy, paste, & use!'
+  metadataBase: new URL('https://ezcopy-buttons.vercel.app'),
+  title: 'EZ COPY',
+  description: 'click. paste. done.',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Buttons, Copy, Easy, Next.js, TailwindCSS, React, Vercel'],
+  creator: 'https://egxo.dev.',
+  manifest: '/manifest.json',
+  robots: {
+    index: true,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
+  openGraph: {
+    title: 'EZ COPY',
+    description: 'Buttons, Copy, Easy, Next.js, TailwindCSS, React, Vercel',
+    url: 'https://devbuild-resume.vercel.app/',
+    siteName: 'ezcopybuttons',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Buttons, Copy, Easy, Next.js, TailwindCSS, React, Vercel'
+      }
+    ],
+    type: 'website',
+    locale: 'en_US'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DevBuild Resume!',
+    description: 'Buttons, Copy, Easy, Next.js, TailwindCSS, React, Vercel',
+    creator: '@eg__xo',
+    site: '@eg__xo',
+    images: [
+      {
+        url: '/twitter-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Buttons, Copy, Easy, Next.js, TailwindCSS, React, Vercel'
+      }
+    ]
+  },
+  icons: {
+    icon: [
+      {url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon'},
+      {url: '/icon.png', sizes: '96x96', type: 'image/png'},
+      {url: '/icon.svg', type: 'image/svg+xml'}
+    ],
+    apple: [{url: '/apple-icon.png', sizes: '180x180', type: 'image/png'}]
+  }
 };
 
 export default function RootLayout({children}) {
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,21 @@
 import styles from './page.module.css';
 import ButtonGrid from '@/components/ButtonGrid';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>
-        <h1>Button Style Showcase</h1>
-        <p>Click any button to copy its CSS code</p>
+        <div>
+          <Image
+            src="/ez-logo.svg"
+            alt="the easy logo"
+            height={150}
+            width={150}
+          />
+          <h1>Button Showcase</h1>
+        </div>
+        <p>Just click, paste, and be done! Give it a try!</p>
       </header>
       <main className={styles.main}>
         <ButtonGrid />
